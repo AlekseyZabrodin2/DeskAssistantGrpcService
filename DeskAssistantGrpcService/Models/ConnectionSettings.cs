3f332f8a6b8585393dbad2f4d0f82fd003267666
ADANI\zabrodin <rediskal50@gmail.com>
2025-11-10 22:37:48 +0300

    feat: create project - gRPC service for DeskAssistent, add NLog logger, app setting

---
+++ DeskAssistantGrpcService/Models/ConnectionSettings.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot.Types;
+
+namespace DeskAssistantGrpcService.Models
+{
+    public class ConnectionSettings
+    {
+        public string DefaultConnection { get; set; } = string.Empty;
+
+        public string BotClientToken { get; set; } = string.Empty;
+
+        public string BotChatId { get; set; } = string.Empty;
+    }
+}
