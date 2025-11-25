8681652c4145160e9a774b8b8726cbc7553dce1d
ADANI\zabrodin <rediskal50@gmail.com>
2025-11-25 17:08:55 +0300

    ref: add extension methods

--- DeskAssistantGrpcService/Services/TaskGrpcService.cs
+++ DeskAssistantGrpcService/Services/TaskGrpcService.cs
@@ -1,6 +1,7 @@
 ﻿using DeskAssistant.Core.Extensions;
 using DeskAssistant.Core.Models;
 using DeskAssistant.Core.Services;
+using DeskAssistantGrpcService.Extensions;
 using Grpc.Core;
 using NLog;
 using ILogger = NLog.ILogger;
@@ -13,6 +14,7 @@ namespace DeskAssistantGrpcService.Services
         private readonly ITaskService _taskService;
         private readonly ITelegramNotificationService _telegramService;
         private EnumExtensions _enumExtensions = new EnumExtensions();
+        private CalendarTasksExtensions _calendarExtensions = new();
 
 
         public TaskGrpcService(ITelegramNotificationService telegramService, ITaskService taskService)
@@ -28,8 +30,8 @@ namespace DeskAssistantGrpcService.Services
             {
                 _logger.Info($"gRPC: Создание задачи '{taskItem.Name}'");
 
-                var taskEntity = GrpcTaskItemToCalendarTaskEntity(taskItem);
-                var taskModel = TaskEntityToCalendarTaskModel(taskEntity);
+                var taskEntity = _calendarExtensions.GrpcTaskItemToCalendarTaskEntity(taskItem);
+                var taskModel = _calendarExtensions.TaskEntityToCalendarTaskModel(taskEntity);
 
                 var createdTask = _taskService.AddTaskForSelectedDate(taskEntity);
 
@@ -59,7 +61,7 @@ namespace DeskAssistantGrpcService.Services
             {
                 var response = new TaskResponse();
 
-                var taskModel = GrpcTaskItemToCalendarTaskModel(taskItem);
+                var taskModel = _calendarExtensions.GrpcTaskItemToCalendarTaskModel(taskItem);
                 var taskStatus = _enumExtensions.StatusFromString(taskModel.Status);
                 await _taskService.UpdateTaskAsync(taskModel, taskStatus);
 
@@ -88,7 +90,7 @@ namespace DeskAssistantGrpcService.Services
                 var allTasks = await _taskService.GetAllTasksAsync();
                 foreach (var item in allTasks)
                 {
-                    var taskItem = CalendarTaskEntityToGrpcTask(item);
+                    var taskItem = _calendarExtensions.CalendarTaskEntityToGrpcTask(item);
 
                     response.Tasks.Add(taskItem);
                 }
@@ -147,117 +149,5 @@ namespace DeskAssistantGrpcService.Services
                 };
             }
         }
-
-        private CalendarTaskModel GrpcTaskItemToCalendarTaskModel(TaskItem taskItem)
-        {
-            return new CalendarTaskModel
-            {
-                Id = string.IsNullOrEmpty(taskItem.Id) ? 0 : int.Parse(taskItem.Id),
-                Name = taskItem.Name,
-                Description = taskItem.Description,
-                DueDate = DateOnly.Parse(taskItem.DueDate),
-                IsCompleted = bool.Parse(taskItem.IsCompleted),
-                Priority = _enumExtensions.PrioritiesLevelFromString(taskItem.Priority),
-                Category = taskItem.Category,
-                Status = taskItem.Status,
-                Tags = taskItem.Tags,
-                CreatedDate = taskItem.CreatedDate == "" ? null : DateTime.SpecifyKind(DateTime.Parse(taskItem.CreatedDate), DateTimeKind.Utc),
-                DueTime = taskItem.DueTime == "" ? null : TimeSpan.Parse(taskItem.DueTime),
-                ReminderTime = taskItem.ReminderTime == "" ? null : DateTime.SpecifyKind(DateTime.Parse(taskItem.ReminderTime), DateTimeKind.Utc),
-                CompletedDate = taskItem.CompletedDate == "" ? null : DateTime.SpecifyKind(DateTime.Parse(taskItem.CompletedDate), DateTimeKind.Utc),
-                IsRecurring = string.IsNullOrEmpty(taskItem.IsRecurring) ? false : bool.Parse(taskItem.IsRecurring),
-                RecurrencePattern = taskItem.RecurrencePattern,
-                Duration = taskItem.Duration == "" ? null : TimeSpan.Parse(taskItem.Duration)
-            };
-        }
-
-        private CalendarTaskEntity GrpcTaskItemToCalendarTaskEntity(TaskItem taskItem)
-        {
-            return new CalendarTaskEntity
-            {
-                Name = taskItem.Name,
-                Description = taskItem.Description,
-                CreatedDate = DateTime.SpecifyKind(DateTime.Parse(taskItem.CreatedDate), DateTimeKind.Utc),
-                DueDate = DateOnly.Parse(taskItem.DueDate),
-                Priority = _enumExtensions.PrioritiesLevelFromString(taskItem.Priority),
-                Category = taskItem.Category,
-                IsCompleted = bool.Parse(taskItem.IsCompleted),
-                Status = _enumExtensions.StatusFromString(taskItem.Status),
-                Tags = taskItem.Tags,
-                RecurrencePattern = "None"
-            };
-        }
-
-        private CalendarTaskModel TaskEntityToCalendarTaskModel(CalendarTaskEntity entity)
-        {
-            return new CalendarTaskModel
-            {
-                Id = entity.Id,
-                Name = entity.Name,
-                Description = entity.Description,
-                DueDate = entity.DueDate,
-                IsCompleted = entity.IsCompleted,
-                Priority = entity.Priority,
-                Category = entity.Category,
-                Status = _enumExtensions.StatusToString(entity.Status),
-                Tags = entity.Tags,
-                CreatedDate = entity.CreatedDate,
-                DueTime = entity.DueTime,
-                ReminderTime = entity.ReminderTime,
-                CompletedDate = entity.CompletedDate,
-                IsRecurring = entity.IsRecurring,
-                RecurrencePattern = entity.RecurrencePattern,
-                Duration = entity.Duration
-            };
-        }
-
-        private TaskItem CalendarTaskModelToGrpcTask(CalendarTaskModel model)
-        {
-            return new TaskItem
-            {
-                Id = model.Id.ToString(),
-                Name = model.Name,
-                Description = model.Description,
-                DueDate = model.DueDate.ToString("yyyy-MM-dd"),
-                IsCompleted = model.IsCompleted.ToString(),
-                Priority = _enumExtensions.PrioritiesLevelToString(model.Priority),
-                Category = model.Category,
-                Status = model.Status,
-                Tags = model.Tags,
-                CreatedDate = model.CreatedDate.ToString(),
-                DueTime = model.DueTime.ToString(),
-                ReminderTime = model.ReminderTime.ToString(),
-                CompletedDate = model.CompletedDate.ToString(),
-                IsRecurring = model.IsRecurring.ToString(),
-                RecurrencePattern = "None",
-                Duration = model.Duration.ToString()
-
-            };
-        }
-
-        private TaskItem CalendarTaskEntityToGrpcTask(CalendarTaskEntity entity)
-        {
-            return new TaskItem
-            {
-                Id = entity.Id.ToString(),
-                Name = entity.Name ?? "",
-                Description = entity.Description ?? "",
-                DueDate = entity.DueDate.ToString("yyyy-MM-dd") ?? "",
-                IsCompleted = entity.IsCompleted.ToString(),
-                Priority = _enumExtensions.PrioritiesLevelToString(entity.Priority) ?? "",
-                Category = entity.Category ?? "",
-                Status = _enumExtensions.StatusToString(entity.Status) ?? "",
-                Tags = entity.Tags ?? "",
-                CreatedDate = entity.CreatedDate.ToString(),
-                DueTime = entity.DueTime?.ToString() ?? "",
-                ReminderTime = entity.ReminderTime?.ToString() ?? "",
-                CompletedDate = entity.CompletedDate?.ToString() ?? "",
-                IsRecurring = entity.IsRecurring.ToString(),
-                RecurrencePattern = "None",
-                Duration = entity.Duration?.ToString() ?? ""
-            };
-        }
-
-
     }
 }
