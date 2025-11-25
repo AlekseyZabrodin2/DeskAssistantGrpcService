d7c8bf80e689a6f005f3bd5c27d6b9a7d3d342f6
ADANI\zabrodin <rediskal50@gmail.com>
2025-11-25 17:09:22 +0300

    ref: registration TelegramGrpcService

--- DeskAssistantGrpcService/Program.cs
+++ DeskAssistantGrpcService/Program.cs
@@ -51,8 +51,9 @@ try
 
     // Configure the HTTP request pipeline.
     app.MapGrpcService<TaskGrpcService>();
+    app.MapGrpcService<TelegramGrpcService>();
     app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+    
     EnsurePostgresRunning();
 
     logger.Info("Service is started");
