59eb5c35b6b41c3dabdc7ed88094352dce80c6b2
ADANI\zabrodin <rediskal50@gmail.com>
2025-12-03 15:00:12 +0300

    feat: create Notification timer

---
+++ DeskAssistantGrpcService/Models/NotificationTimer.cs
@@ -0,0 +1,13 @@
+﻿using DeskAssistant.Core.Models;
+
+namespace DeskAssistantGrpcService.Models
+{
+    public class NotificationTimer
+    {
+        public Guid Id { get; set; }
+        public string NotificationId { get; set; }
+        public Timer Timer { get; set; }
+        public DateTime ScheduledTime { get; set; }
+        public NotificationEntity Notification { get; set; }
+    }
+}
